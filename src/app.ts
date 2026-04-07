a3fe47dbe04fd2bda6557eb5de203c185d3a8948
mchen1107 <chen.xum@northeastern.edu>
2026-04-07 13:04:53 -0700

    update

--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,12 @@ app.use(express.json());
   return this.toString();
 };
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("API is running");
 });
 
-app.use("/territories", territoryRoutes);
-app.use("/ext", extRoutes);
-app.use("/curated", curatedRoutes);
+app.use("/api/territories", territoryRoutes);
+app.use("/api/ext", extRoutes);
+app.use("/api/curated", curatedRoutes);
 
 export default app;
