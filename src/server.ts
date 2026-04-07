701a7e4abdbcd037f48513a64070b6adcb9a7915
mchen1107 <chen.xum@northeastern.edu>
2026-04-07 10:35:27 -0700

    Fix server: always listen on port

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import app from "./app.js";
 
-const port = process.env.PORT || 3001;
+const port = parseInt(process.env.PORT || "3001");
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 export default app;
