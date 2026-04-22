859321a01196409759bb63c9263dc68aee267120
mchen1107 <chen.xum@northeastern.edu>
2026-04-21 20:40:00 -0700

    update connection settings

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
-  port: Number(process.env.DB_PORT ?? 3306),
+  socketPath: process.env["DB_SOCKET"] ?? "/var/lib/mysql/mysql.sock",
+  user: process.env["DB_USER"] ?? "",
+  password: process.env["DB_PASSWORD"] ?? "",
+  database: process.env["DB_NAME"] ?? "",
   waitForConnections: true,
   connectionLimit: 10,
 });
